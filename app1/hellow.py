d308b48ecd0ea75041a2ae20fe09f23e9070fe36
Ashishverma001 <111993809+Ashishverma001@users.noreply.github.com>
2022-11-13 15:17:58 +0530

    streamlit

--- app1/hellow.py
+++ app1/hellow.py
@@ -1,2 +1,22 @@
 # streamlit run app/hellow.py
-import streamlit as st 
+import streamlit as st
+from PIL import Image 
+
+st.title("Sample App")
+
+image = st.camera_input("Take a picture")
+if image:
+    im = Image.open(image)
+
+    color = st.color_picker("Pick a color", "#00f900")
+
+    im2  = Image.new("RGB", im.size, color)
+    
+    im = Image.blend(im, im2, 0.5)
+    
+    st.sidebar.image(im)
+    
+    filename = st.text_input("Save as", "image.png")
+    if st.button("Save"):
+        im.save(filename)
+        st.snow()
